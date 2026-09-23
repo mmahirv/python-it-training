9472d4f18877931c7de021746782ebc6f2b76b1e
mmahirv <mmahirv@hotmail.com>
2026-09-23 19:30:51 +0200

    library structure created

---
+++ python/week-03/project/library_app/api_lookup.py
@@ -0,0 +1,7 @@
+import requests
+
+
+def lookup_book(title):
+    #Retrieves the author and year information for the book via the Open Library API.
+    # return type {"author": author, "year": year} 
+    return None

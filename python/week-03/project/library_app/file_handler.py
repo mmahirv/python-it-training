e8ef6ae94fabca80900f05319534963e0b6bfb23
mmahirv <mmahirv@hotmail.com>
2026-09-23 19:08:52 +0200

    file handler  structure added

---
+++ python/week-03/project/library_app/file_handler.py
@@ -0,0 +1,23 @@
+import csv
+import json
+import os
+
+
+def load_library(filepath):
+    #Loads the book list from a JSON file. Safely returns an empty list if the file is missing or corrupt.
+    return None
+
+def save_library(filepath, books):
+    #Saves the book list to a JSON file in a readable format (indent=4).
+
+    return None
+
+def export_to_csv(books, filepath):
+    #Exports the book list in CSV format.
+
+    return None
+
+def import_from_csv(filepath):
+    #Imports the book list in CSV format.
+
+    return None

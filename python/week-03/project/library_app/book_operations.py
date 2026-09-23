bdf07a4af95183e2912d035a60199b34794c4aa1
mmahirv <mmahirv@hotmail.com>
2026-09-23 19:04:55 +0200

    book operatinons structur added

---
+++ python/week-03/project/library_app/book_operations.py
@@ -0,0 +1,20 @@
+def add_book(books):
+    #Taking book infos from user and check the year of the book after add list
+    return None
+
+def view_books(books):
+    #Show the books names to user good format
+    
+    return None
+
+
+def search_books(books):
+    #Show the books from author or from book name given by user 
+        
+    return None
+
+
+def delete_book(books):
+    #Delete book taking from user book name
+
+    return None

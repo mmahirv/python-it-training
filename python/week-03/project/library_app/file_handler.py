85b4225074ae5f60940f67b4ba0760e130909c5b
mmahirv <mmahirv@hotmail.com>
2026-09-24 21:29:30 +0200

    file explanations

--- python/week-03/project/library_app/file_handler.py
+++ python/week-03/project/library_app/file_handler.py
@@ -5,6 +5,8 @@ import os
 
 def load_library(filepath):
     #Loads the book list from a JSON file. Safely returns an empty list if the file is missing or corrupt.
+    #return list of book if books exists else return empty list []
+    
     return None
 
 def save_library(filepath, books):
@@ -19,5 +21,6 @@ def export_to_csv(books, filepath):
 
 def import_from_csv(filepath):
     #Imports the book list in CSV format.
+    #return list of book if books exists else return empty list []
 
     return None
